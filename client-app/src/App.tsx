9af84284b687ac702e96bdae210970203b34366e
Bertelli-Gianluigi <bertelli_gianluigi@bah.com>
2023-07-24 15:46:15 -0400

    This version works from the terminals. Need to fix docker

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -10,20 +11,18 @@ function App() {
     axios.get('http://localhost:5000/api/activities').then(response => {
     console.log(response)  
     setActivities(response.data)
-    }).catch((err) => console.error(err))
+    })//.catch((err) => console.error(err))
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <ul>
+    <div >
+      <Header as='h2' icon='users' content='Reactivities' />
+       <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>
+          <List.Item key={activity.id}>
             {activity.title}
-          </li>
+          </List.Item>
         ))}
-       </ul>
-      </header>
+       </List>
     </div>
   );
 }
